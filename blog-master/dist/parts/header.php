b32a64049c29b3ecb54f12f312a78e47e8700605
smaile02 <smaile02@gmail.com>
2018-02-22 00:15:57 +0200

    ikelimas1

---
+++ blog-master/dist/parts/header.php
@@ -0,0 +1,21 @@
+<header>
+  <div class="contacts-menu">
+    <div class="social">
+      <a href="https://facebook.com">
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+      </a>
+    </div>
+    <div class="contacts">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <p>laikas</p>
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <a href="tel:+370898888">+370 89 8888</a>
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <a href="mailto:info@google.com">info@google.com</a>
+    </div>
+  </div>
+  <div class="navigation-menu">
+
+  </div>
+
+</header>
